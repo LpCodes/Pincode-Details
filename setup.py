e7c9aded65eab276d180ebee392e65223b666c4c
LP <lovelesh_p@yahoo.com>
2022-06-14 17:24:55 +0530

    setup

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
