c2a5e436dfa0bf975f5c817891483b447e3305cb
LP <lovelesh_p@yahoo.com>
2022-06-14 16:54:51 +0530

    Few changes in setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="IndianPinCodes",
+    version="1.0.0",
+    author="Lpcodes",
+    description="A package for fetching details for the pincode provided",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LpCodes/Pincode-Details",
+    project_urls={
+        "Bug Tracker": "https://github.com/LpCodes/Pincode-Details/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
