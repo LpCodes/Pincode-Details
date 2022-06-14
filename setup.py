38119ec0f80b99164a70cefb6d96e4ad1268e6e0
LP <lovelesh_p@yahoo.com>
2022-06-14 17:24:24 +0530

    restructuring

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
